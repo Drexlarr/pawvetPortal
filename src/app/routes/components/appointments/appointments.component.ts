a7ee5c8d212e60fb9a7b646738813a60373f0f11
xanderMMR2 <alexandermelendezrios@gmail.com>
2024-05-13 23:52:38 -0500

    Segunda versión

--- src/app/routes/components/appointments/appointments.component.ts
+++ src/app/routes/components/appointments/appointments.component.ts
@@ -22,6 +22,7 @@ export class AppointmentsComponent {
     { nombre: 'Alexander Meléndez', tipoConsulta: 'Consulta general', fecha: '2024-05-14', hora: '10:00 AM', img:'https://upload.wikimedia.org/wikipedia/commons/5/58/Shiba_inu_taiki.jpg'},
     { nombre: 'Alexander Meléndez', tipoConsulta: 'Consulta general', fecha: '2024-05-14', hora: '10:00 AM', img:'https://upload.wikimedia.org/wikipedia/commons/5/58/Shiba_inu_taiki.jpg'},
     { nombre: 'Alexander Meléndez', tipoConsulta: 'Consulta general', fecha: '2024-05-14', hora: '10:00 AM', img:'https://upload.wikimedia.org/wikipedia/commons/5/58/Shiba_inu_taiki.jpg'},
+    { nombre: 'Alexander Meléndez', tipoConsulta: 'Consulta general', fecha: '2024-05-14', hora: '10:00 AM', img:'https://upload.wikimedia.org/wikipedia/commons/5/58/Shiba_inu_taiki.jpg'},
     { nombre: 'Diego Bazán', tipoConsulta: 'Consulta de emergencia', fecha: '2024-05-13', hora: '11:00 AM',img:'https://i.pinimg.com/236x/40/2f/73/402f7382d9bb9731e6cbf2da1d0822a6.jpg' },
     { nombre: 'Dina Boluarte', tipoConsulta: 'Consulta especializada', fecha: '2024-05-15', hora: '12:00 PM',img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJABV5wACGI_pHTTObF0UetjmE4Ah7qHBDA&s' },
     { nombre: 'Dina Boluarte', tipoConsulta: 'Consulta especializada', fecha: '2024-05-15', hora: '12:00 PM',img:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThJABV5wACGI_pHTTObF0UetjmE4Ah7qHBDA&s' }
